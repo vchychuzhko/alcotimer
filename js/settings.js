738f4d90da35ccaaa1a3903db3daac06a4aae63d
Vladyslav Chychuzhko <vlad.chichuzko@gmail.com>
2019-03-24 23:00:49 +0200

    Settings are now applied on the opening the page. Menu is now get closed by saving the configurations.

--- js/settings.js
+++ js/settings.js
@@ -6,6 +6,7 @@
          */
         _create: function () {
             this.initBindings();
+            this.applySettings(false);
         },
 
         /**
@@ -14,38 +15,53 @@
         initBindings: function () {
             let $saveButton = $(this.element).find('.save-button'),
                 $resetButton = $(this.element).find('.reset-button'),
-                $minTimeInput = $(this.element).find('.min-value.time'),
-                $minTimeRange = $(this.element).find('.min-range.time'),
-                $maxTimeInput = $(this.element).find('.max-value.time'),
-                $maxTimeRange = $(this.element).find('.max-range.time'),
-                $toggleContainer = $(this.element).find('.toggle-container');
+                $toggleContainer = $(this.element).closest('.menu').find('.toggle-container');
 
             $saveButton.on('click', function () {
-                let settings = {
-                    'minTime': $minTimeInput.val(),
-                    'maxTime': $maxTimeInput.val()
-                };
-
-                localStorage.settings = JSON.stringify(settings);
+                this.saveSettings();
                 $toggleContainer.trigger('click');
             }.bind(this));
 
-            $resetButton.on('click', function () {
-                let settings = JSON.parse(localStorage.getItem('settings'));
-
-                if (settings !== null) {
-                    $minTimeInput.val(settings.minTime);
-                    $minTimeRange.val(settings.minTime);
-                    $maxTimeInput.val(settings.maxTime);
-                    $maxTimeRange.val(settings.maxTime);
-                } else {
-                    this.showMessage('Settings were not found. Please, save new configurations.', true);
-                }
-            }.bind(this));
+            $resetButton.on('click', this.applySettings.bind(this));
         },
 
         /**
-         *
+         * Apply settings from the local storage
+         * @param showError
+         */
+        applySettings: function (showError = true) {
+            let settings = JSON.parse(localStorage.getItem('settings')),
+                $minTimeInput = $(this.element).find('.min-value.time'),
+                $minTimeRange = $(this.element).find('.min-range.time'),
+                $maxTimeInput = $(this.element).find('.max-value.time'),
+                $maxTimeRange = $(this.element).find('.max-range.time');
+
+            if (settings !== null) {
+                $minTimeInput.val(settings.minTime);
+                $minTimeRange.val(settings.minTime);
+                $maxTimeInput.val(settings.maxTime);
+                $maxTimeRange.val(settings.maxTime);
+            } else if (showError) {
+                this.showMessage('Settings were not found. Please, save new configurations.', true);
+            }
+        },
+
+        /**
+         * Save settings to the local storage
+         */
+        saveSettings: function () {
+            let $minTimeInput = $(this.element).find('.min-value.time'),
+                $maxTimeInput = $(this.element).find('.max-value.time'),
+                settings = {
+                'minTime': $minTimeInput.val(),
+                'maxTime': $maxTimeInput.val()
+            };
+
+            localStorage.settings = JSON.stringify(settings);
+        },
+
+        /**
+         * Show message in the bottom of settings
          * @param message
          * @param isError
          */
